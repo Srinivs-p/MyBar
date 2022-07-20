2884072b5dc94187830328b5f5b44aa4d2779c77
Srinivas <51451285+Srinivs-p@users.noreply.github.com>
2022-07-20 16:34:04 -0400

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,19 @@
+window.addEventListener("load",function(){
+	 setTimeout( function open(event){
+		document.querySelector(".popup").style.display = "block";
+	 },1000
+
+	)
+})
+
+document.querySelector('#close').addEventListener("click", function() {
+	document.querySelector('.popup').style.display = "none";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.popup').style.display = "none";
+});
+
+function closeWindow() {
+	newWindow.close();
+  }
